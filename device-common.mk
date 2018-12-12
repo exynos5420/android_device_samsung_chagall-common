d7b0a27db99c93fc81cf3bacf6a8c07980fea35e
Anton Hansson <hansson@google.com>
2018-12-12 16:15:21 +0000

    klimt: Remove PRODUCT_AAPT config
    This config strips unused drawable resources from the device
    to save space, but it is not particularly effective in practice
    (roughly ~30 MB across all partitions).
    
    Remove it to make the system image more generic.
    
    Bug: 118676832
    Test: make
    Change-Id: I3ae120773e793af6e7b5759d383b7fa9c65aeb48

--- device-common.mk
+++ device-common.mk
@@ -24,12 +24,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     device/samsung/chagall-common/overlay/lineage-sdk \
     device/samsung/chagall-common/overlay/hardware/samsung/AdvancedDisplay
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := xlarge
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-# A list of dpis to select prebuilt apk, in precedence order.
-PRODUCT_AAPT_PREBUILT_DPI := hdpi mdpi
-
 # AdvancedDisplay (MDNIE)
 PRODUCT_PACKAGES += \
     AdvancedDisplay
