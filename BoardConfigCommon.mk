296aaa3bbee5038a259255e283f413d8d20a509c
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-02-25 18:10:05 +0100

    chagall: override SDK api level for vcsFPService
        * triggers
    
    	Abort message: 'invalid pthread_t 0xa8de2230 passed to pthread_kill'

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,5 +31,9 @@ TARGET_SCREEN_DENSITY := 320
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Legacy blobs support
+TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
+    /system/vendor/bin/vcsFPService=22
+
 # Inherit from the proprietary version
 -include vendor/samsung/chagall-common/BoardConfigVendor.mk
