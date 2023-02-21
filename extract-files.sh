96336ad2d88a0721cdec81564092af9bacd285a3
Michael Bestas <mkbestas@lineageos.org>
2023-02-21 06:34:52 +0000

    chagall-common: : Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: I5e956c75ba730c29dc5a72b25eca507f1e81c179

--- extract-files.sh
+++ extract-files.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-DEVICE_COMMON=chagall-common
-VENDOR=samsung
-
-# Load extractutils and do some sanity checks
+# Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
@@ -26,11 +24,19 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
+ONLY_COMMON=
+ONLY_TARGET=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
+        --only-common )
+                ONLY_COMMON=true
+                ;;
+        --only-target )
+                ONLY_TARGET=true
+                ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
                 ;;
@@ -52,22 +58,42 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-# Initialize the helper
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+function blob_fixup() {
+    case "${1}" in
+        etc/wifi/nvram_mfg.txt_4354_a0)
+            sed -i 's|ccode=ALL|ccode=00|g' "${2}"
+            ;;
+        etc/wifi/nvram_mfg.txt_4354_a0)
+            sed -i 's|regrev=0|regrev=6|g' "${2}"
+            ;;
+	etc/wifi/nvram_mfg.txt_4354_a1)
+            sed -i 's|ccode=ALL|ccode=00|g' "${2}"
+	    ;;
+	etc/wifi/nvram_mfg.txt_4354_a1)
+            sed -i 's|regrev=0|regrev=6|g' "${2}"
+	    ;;
+        etc/wifi/nvram_net.txt_4354_a0)
+            sed -i 's|ccode=GB|ccode=00|g' "${2}"
+	    ;;
+        etc/wifi/nvram_net.txt_4354_a1)
+            sed -i 's|ccode=GB|ccode=00|g' "${2}"
+	    ;;
+    esac
+}
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+if [ -z "${ONLY_TARGET}" ]; then
+    # Initialize the helper for common device
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-# Fix proprietary blobs
-BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
 
-# nvram_mfg.txt
-sed -i 's|ccode=ALL|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4354_a0
-sed -i 's|regrev=0|regrev=6|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4354_a0
-sed -i 's|ccode=ALL|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4354_a1
-sed -i 's|regrev=0|regrev=6|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4354_a1
+if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    source "${MY_DIR}/../${DEVICE}/extract-files.sh"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-# nvram_net.txt
-sed -i 's|ccode=GB|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_net.txt_4354_a0
-sed -i 's|ccode=GB|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_net.txt_4354_a1
+    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
 
 "${MY_DIR}/setup-makefiles.sh"
